947a732c292b1f0ebe8a44f52573c33c1c90a0eb
ayush21roy <4nn20cs012@nieit.ac.in>
2025-01-29 01:48:11 +0530

    - eventListener for all cellProp (bold,Italic, underline etc..) - added html-class-css

--- cell-properties.js
+++ cell-properties.js
@@ -46,8 +46,93 @@ bold.addEventListener("click", (e) => {
     // Modification
     cellProp.bold = !cellProp.bold; // Data change
     cell.style.fontWeight = cellProp.bold ? 'bold' : 'normal'; // UI change(1)
-    bold.style.backgroundColor = cellProp.bold ? activeColorProp : inactiveColorProp;
+    bold.style.backgroundColor = cellProp.bold ? activeColorProp : inactiveColorProp; // UI change(2)
 });
+
+italic.addEventListener("click", (e) => {
+    let address = addressBar.value;
+    let [cell, cellProp] = activeCell(address); // destructing
+
+    // Modification
+    cellProp.italic = !cellProp.italic; // Data change
+    cell.style.fontStyle = cellProp.italic ? 'italic' : 'normal'; // UI change(1)
+    italic.style.backgroundColor = cellProp.italic ? activeColorProp : inactiveColorProp;
+});
+
+underline.addEventListener("click", (e) => {
+    let address = addressBar.value;
+    let [cell, cellProp] = activeCell(address); // destructing
+
+    // Modification
+    cellProp.underline = !cellProp.underline; // Data change
+    cell.style.textDecoration = cellProp.underline ? 'underline' : 'none'; // UI change(1)
+    underline.style.backgroundColor = cellProp.underline ? activeColorProp : inactiveColorProp;
+});
+
+fontSize.addEventListener("change", (e) => {
+    let address = addressBar.value;
+    let [cell, cellProp] = activeCell(address);
+
+    cellProp.fontSize = fontSize.value; // Data change
+    cell.style.fontSize = cellProp.fontSize + "px";
+    fontSize.value = cellProp.fontSize;
+});
+
+fontFamily.addEventListener("change", (e) => {
+    let address = addressBar.value;
+    let [cell, cellProp] = activeCell(address);
+
+    cellProp.fontFamily = fontFamily.value; // Data change
+    cell.style.fontFamily = cellProp.fontFamily;
+    fontFamily.value = cellProp.fontFamily;
+});
+
+fontColor.addEventListener("change", (e) => {
+    let address = addressBar.value;
+    let [cell, cellProp] = activeCell(address);
+
+    cellProp.fontColor = fontColor.value; // Data change
+    cell.style.color = cellProp.fontColor;
+    fontColor.value = cellProp.fontColor;
+});
+
+BGcolor.addEventListener("change", (e) => {
+    let address = addressBar.value;
+    let [cell, cellProp] = activeCell(address);
+
+    cellProp.BGcolor = BGcolor.value; // Data change
+    cell.style.backgroundColor = cellProp.BGcolor;
+    BGcolor.value = cellProp.BGcolor;
+});
+
+alignment.forEach((alignElem) => {      // forEach coz its a NodeList querySelAll
+    alignElem.addEventListener("click", (e) => {
+        let address = addressBar.value;
+        let [cell, cellProp] = activeCell(address);
+
+        let alignValue = e.target.classList[0]; // class="left material-symbols-outlined alignment" -> left
+        cellProp.alignment = alignValue; // Data change
+        cell.style.alignment = cellProp.alignment // UI change (1)
+
+        switch(alignValue) { // UI change (2)
+            case 'left':
+                leftAlign.style.backgroundColor = activeColorProp;
+                centerAlign.style.backgroundColor = inactiveColorProp;
+                rightAlign.style.backgroundColor = inactiveColorProp;
+                break;
+            case 'center':
+                leftAlign.style.backgroundColor = inactiveColorProp;
+                centerAlign.style.backgroundColor = activeColorProp;
+                rightAlign.style.backgroundColor = inactiveColorProp;
+                break;
+            case 'right':
+                leftAlign.style.backgroundColor = inactiveColorProp;
+                centerAlign.style.backgroundColor = inactiveColorProp;
+                rightAlign.style.backgroundColor = activeColorProp;
+                break;
+        }
+    })
+})
 
 function activeCell(address) {
     let [rid, cid] = decodeRIDCIDFromAddress(address); //way is called arr-destructuring
